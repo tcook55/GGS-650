310e1e36b7e43f45602743556f42a418bc770460
tcook55 <43211149+tcook55@users.noreply.github.com>
2018-10-05 12:43:32 -0400

    Update Assignment_5-3.py

--- Assignment_5-3.py
+++ Assignment_5-3.py
@@ -15,16 +15,33 @@ class Polygon:
 def get_LineSegments(poly):
     line_lis = []
     ctr = 0
-    while ctr < len(poly.points) + 1:
-        if ctr == len(poly.points) - 1:
+    while ctr <= len(poly.points) - 1:
+        if ctr <= len(poly.points) - 2:
             line = LineSegment(poly.points[ctr], poly.points[ctr + 1])
             line_lis.append(line)
             ctr += 1
-        elif ctr == len(poly.points):
+        else:
             line = LineSegment(poly.points[ctr], poly.points[0])
-            print (line_lis[3].start_point.x)
-    
+            line_lis.append(line)
+            ctr += 1
     return line_lis
+
+def PIP(poly, point):
+    max_x = 0
+    for iter in poly.points:
+        if iter.x > max_x:
+            max_x = iter.x
+    check_point = Point(max_x, point.y)
+    check_line = LineSegment(point, check_point)
+    lines = get_LineSegments(poly)
+    cross_check = 0
+    for check in lines:
+        if cross(check, check_line) == 1:
+            cross_check += 1
+    if cross_check % 2 == 0 and cross_check != 0:
+        return 0
+    else:
+        return 1
 
 def rhs(a, b, c):
     originx = a.x
@@ -60,17 +77,21 @@ def cross(a, b):
     else:
         return 1
     
-my_point1 = Point(0,5)
-my_point2 = Point(9,13)
-my_point3 = Point(1,6)
-my_point4 = Point(7,2)
-my_point5 = Point(4,10)
+my_point1 = Point(10,1)
+my_point2 = Point(8,12)
+my_point3 = Point(-1,4)
+my_point4 = Point(-5,-3)
+my_point5 = Point(6,-4)
+
+interior_point = Point(6,3)
+exterior_point = Point(12,5)
+
+#Interior point is (6, 3)
+#Exterior point is (12, 5)
 
 line1 = LineSegment(my_point1, my_point2)
 line2 = LineSegment(my_point3, my_point4)
 
 points = [my_point1, my_point2, my_point3, my_point4, my_point5]
 poly = Polygon(points)
-print (get_LineSegments(poly))
-
-
+print (PIP(poly, interior_point))
